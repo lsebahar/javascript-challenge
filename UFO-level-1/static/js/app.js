7d94d103193fd6bd62a450d29a67ad5bc1f4355d
Levi Sebahar <leviseb@gmail.com>
2020-05-14 22:39:59 -0700

    Completed code.

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -20,31 +20,14 @@ function runFilter() {
     // Get the value property of the input element
     var inputValue = inputElement.property("value");
 
+    //filter the data to match the input element
     var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
 
-
-    // for (i = 1; i < filteredData.length +1; i++) {
+    //select the table body to add rows
     var table = d3.select('tbody');
     table.html('');
-    //     var row = table.append("tr");
-    //     table.html('');
 
-    //     var cell0 = row.insertCell(0);
-    //     var cell1 = row.insertCell(1);
-    //     var cell2 = row.insertCell(2);
-    //     var cell3 = row.insertCell(3);
-    //     var cell4 = row.insertCell(4);
-    //     var cell5 = row.insertCell(5);
-    //     var cell6 = row.insertCell(6);
-
-    //     cell0.innerHTML = filteredData[i].datetime;
-    //     cell1.innerHTML = filteredData[i].city;
-    //     cell2.innerHTML = filteredData[i].state;
-    //     cell3.innerHTML = filteredData[i].country;
-    //     cell4.innerHTML = filteredData[i].shape;
-    //     cell5.innerHTML = filteredData[i].durationMinutes;
-    //     cell6.innerHTML = filteredData[i].comments;
-
+    //adding row for each record of data
     filteredData.forEach((sighting) => {
         //console.log(sighting);
         var row = table.append("tr");
